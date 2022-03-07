8eca4db4ee9e8c9de97e917b52f36889c1eabd30
RanSelf <thisiswr@ourlook.com>
2022-03-08 02:40:20 +0800

    add code

---
+++ 48.旋转图像.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=java
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+class Solution {
+
+    public void rotate(int[][] matrix) {
+        var n = matrix.length;
+        int[][] temp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = temp[i][j];
+            }
+        }
+    }
+
+}
+// @lc code=end
